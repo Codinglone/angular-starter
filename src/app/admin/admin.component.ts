dfceb3e8bc53c4646ae3441e6c61f1acd373c2ef
codinglone <codinglone@gmail.com>
2023-06-18 17:31:01 +0400

    Created admin component

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.css']
+})
+
+
+export class AdminComponent{
+    
+}
