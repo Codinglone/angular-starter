99c28d776510e04be4ca1d1de9ffcb823433dc84
codinglone <codinglone@gmail.com>
2023-06-18 17:26:06 +0400

    Customized component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-start';
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-auth',
+  selector: 'app-user',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
